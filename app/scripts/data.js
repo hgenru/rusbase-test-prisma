a67cc1c946e8fb9030d793c7d5a77d31f8ad8d3b
Александр Плесовских <me@hgen.ru>
2016-04-30 19:13:59 +0300

    ебитесь конём, абсолют непобедим

---
+++ app/scripts/data.js
@@ -0,0 +1,24 @@
+window.DATA = [
+    {
+        title: 'Что закрашено?',
+        picture_name: '1-banya',
+        correct_answer: 2,
+        answers: [
+            'Это муж',
+            'Это жена',
+            'Это кролик',
+            'Это дерево'
+        ]
+    },
+    {
+        title: 'Что закрашено?',
+        picture_name: '2-crolic',
+        correct_answer: 2,
+        answers: [
+            'Это 1',
+            'Это 2',
+            'Это 3',
+            'Это 4'
+        ]
+    }
+];
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,27 @@
-var ko = window.ko;
+(function() {
+    'use strict';
+    var ko = window.ko;
+    var modelExtend = window.modelExtend;
 
-function AppViewModel() {
-    this.currentPage = ko.observable('main');
+    function QuestionViewModel(props) {
+        this.title = null;
+        this.answers = [];
+        this.selected = ko.observable();
 
-    this.currentQuestion = ko.observable();
-    this.questions = ko.observableArray();
-}
+        this.picture_hide = 'resources/quiz-pictures/' +
+            props.picture_name + '-hide.jpg';
+        modelExtend(this, props);
+    }
 
-window.m_site = new AppViewModel();
-ko.applyBindings(window.m_site);
+    function AppViewModel() {
+        this.currentPage = ko.observable('main');
+
+        this.currentQuestion = ko.observable(0);
+        this.questions = ko.observableArray(window.DATA.map(function(q) {
+            return new QuestionViewModel(q);
+        }));
+    }
+
+    window.m_site = new AppViewModel();
+    ko.applyBindings(window.m_site);
+})();
