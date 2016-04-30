da247562cc89ab028643abb3730de7efc74f02c2
Александр Плесовских <me@hgen.ru>
2016-04-30 19:45:08 +0300

    up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,12 +6,29 @@
     function QuestionViewModel(props) {
         this.title = null;
         this.answers = [];
-        this.selected = ko.observable();
+        this.correct_answer = null;
+        this.selected = ko.observable(-1);
+
+        this.isSelected = ko.pureComputed(function() {
+            return this.selected() > -1;
+        }, this);
+
+        this.answerIsCorrect = ko.pureComputed(function() {
+            var selected = ko.unwrap(this.selected);
+            return selected === this.correct_answer;
+        }, this);
 
         this.picture_hide = 'resources/quiz-pictures/' +
             props.picture_name + '-hide.jpg';
+        this.picture_result = 'resources/quiz-pictures/' +
+            props.picture_name + '.jpg';
         modelExtend(this, props);
     }
+
+    QuestionViewModel.prototype.selectAnswer = function(answerIndex) {
+        console.log('wtf', answerIndex)
+        this.selected(answerIndex);
+    };
 
     function AppViewModel() {
         this.currentPage = ko.observable('main');
